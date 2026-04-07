1245c694112db3418f9d7a057e4c78339f78bec8
alex-jga <jaouena8@gmail.com>
2026-04-07 15:57:04 +0200

    Finished programming Morpion Game

--- Tic_Tac_Toe/morpion_graven.py
+++ Tic_Tac_Toe/morpion_graven.py
@@ -1,114 +1,137 @@
-import tkinter
-def print_winner():
-    global win
+import tkinter 
 
-    if win is False:
-        win = True
+def set_tile(row, column):
+    global curr_player
 
-    window_win = tkinter.Tk()
-    window_win.title("Victoire")
+    if (game_over):
+        return
 
-    texte = f"Le joueur {current_player} a gagné le jeu"
+    if board[row][column]["text"] != "":
+        return
+    
+    board[row][column]["text"] = curr_player
 
-    label = tkinter.Label(window_win, text=texte)
-    label.pack()
-
-    window_win.mainloop()
-       
-       
-
-def switch_player():
-    global current_player
-    if current_player == 'X':
-        current_player = '0'
+    if curr_player == playerO: 
+        curr_player = playerX
     else:
-        current_player = 'X'
-
-def check_win(clicked_row, clicked_col):
-
-    # detecter victoire horizontale
-    count = 0
-    for i in range(3):
-        current_button = buttons[i][clicked_row]
-        if current_button['text'] == current_player:
-            count += 1
-    if count == 3:
-        print_winner()
-
-    # detecter victoire verticale
-    count = 0
-    for i in range(3):
-        current_button = buttons[clicked_col][i]
-        if current_button['text'] == current_player:
-            count += 1
-    if count == 3:
-        print_winner()
-
-    # detecter victoire diagonale
-    count = 0
-    for i in range(3):
-        current_button = buttons[i][i]
-        if current_button['text'] == current_player:
-            count += 1
-    if count == 3:
-        print_winner()
-
-    # detecter victoire diagonale inversee
-    count = 0
-    for i in range(3):
-        current_button = buttons[2-i][i]
-        if current_button['text'] == current_player:
-            count += 1
-    if count == 3:
-        print_winner()
-
-    if win is False:
-        count = 0
-        for col in range(3):
-            for row in range(3):
-                current_button = buttons[col][row]
-                if current_button['text'] == 'X' or current_button['text'] == '0':
-                    count += 1
-        if count == 9:
-            print("Match nul")
+        curr_player = playerO
+    
+    label["text"] = " Au tour de " + curr_player
 
 
-def place_symbol(row, column):
+    check_winner()
 
-    clicked_button = buttons[column][row]
-    if clicked_button['text'] == "":
-        clicked_button.config(text=current_player)
+def check_winner():
+    global turns, game_over
+    turns += 1
 
-        check_win(row, column)
-        switch_player()
 
-def draw_grid():
+    for row in range(3):
+        if (board[row][0]["text"] == board[row][1]["text"] == board[row][2]["text"]
+            and board[row][0]["text"] != ""):
+            label.config(text=board[row][0]["text"]+" a gagné!", foreground=color_yellow)
+            for column in range(3):
+                board[row][column].config(foreground=color_yellow, background=color_light_gray)
+            game_over = True
+            return
+    
+
     for column in range(3):
-        buttons_in_cols = []
-        for row in range(3):
-            button = tkinter.Button(
-                root, font=("Arial", 50),
-                width=5, height=2,
-                command=lambda r=row, c=column: place_symbol(r, c)
-            )
-            button.grid(row=row, column=column)
-            buttons_in_cols.append(button)
-        buttons.append(buttons_in_cols)
-
-# stockages
-buttons = []
-current_player = 'X'
-win = False
-
-# creer la fenetre du jeu
-root = tkinter.Tk()
-
-# creer la fenetre du gagnant
+        if (board[0][column]["text"] == board[1][column]["text"] == board[2][column]["text"]
+            and board[0][column]["text"] != ""):
+            label.config(text=board[0][column]["text"]+" a gagné!", foreground=color_yellow)
+            for row in range(3):
+                board[row][column].config(foreground=color_yellow, background=color_light_gray)
+            game_over = True
+            return
 
 
-# personnalisation de la fenetre
-root.title("TicTacToe")
-root.minsize(500, 500)
+    if (board[0][0]["text"] == board[1][1]["text"] == board[2][2]["text"]
+        and board[0][0]["text"] != ""):
+        label.config(text=board[0][0]["text"]+" a gagné!", foreground=color_yellow)
+        for i in range(3):
+            board[i][i].config(foreground=color_yellow, background=color_light_gray)
+        game_over = True
+        return
 
-draw_grid()
-root.mainloop()
+
+    if (board[0][2]["text"] == board[1][1]["text"] == board[2][0]["text"]
+        and board[0][2]["text"] != ""):
+        label.config(text=board[0][2]["text"]+"  a gagné!", foreground=color_yellow)
+        board[0][2].config(foreground=color_yellow, background=color_light_gray)
+        board[1][1].config(foreground=color_yellow, background=color_light_gray)
+        board[2][0].config(foreground=color_yellow, background=color_light_gray)
+        game_over = True
+        return
+    
+ 
+    if (turns == 9):
+        game_over = True
+        label.config(text="Egalité !", foreground=color_yellow)
+
+
+def new_game():
+    global turns, game_over
+
+    turns = 0
+    game_over = False
+
+    label.config(text=" Au tour de " + curr_player, foreground="white")
+
+    for row in range(3):
+        for column in range(3):
+            board[row][column].config(text="", foreground=color_blue, background=color_gray)
+
+
+
+playerX = "X"
+playerO = "O"
+curr_player = playerX
+board = [[0, 0, 0], 
+         [0, 0, 0], 
+         [0, 0, 0]]
+
+color_blue = "#4584b6"
+color_yellow = "#ffde57"
+color_gray = "#343434"
+color_light_gray = "#646464"
+
+turns = 0
+game_over = False
+
+window = tkinter.Tk()
+window.title("Tic Tac Toe")
+window.resizable(False, False)
+
+frame = tkinter.Frame(window)
+label = tkinter.Label(frame, text=" Au tour de " + curr_player, font=("Consolas", 20), background=color_gray,
+                      foreground="white")
+label.grid(row=0, column=0, columnspan=3, sticky="we")
+
+for row in range(3):
+    for column in range(3):
+        board[row][column] = tkinter.Button(frame, text="", font=("Consolas", 50, "bold"),
+                                            background=color_gray, foreground=color_blue, width=4, height=1,
+                                            command=lambda row=row, column=column: set_tile(row, column))
+        board[row][column].grid(row=row+1, column=column)
+
+button = tkinter.Button(frame, text="Rejouer", font=("Consolas", 20), background=color_gray,
+                        foreground="white", command=new_game)
+button.grid(row=4, column=0, columnspan=3, sticky="we")
+
+frame.pack()
+
+
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+
+
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+window.mainloop()
